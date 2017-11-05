27ee18e8c0dad9ff87789e597f5779dbc4d5a283
Boyko Boykov <boyko@receipt-bank.com>
2017-11-05 10:06:15 +0200

    Add Convert Github clones to use HTTPS/SSH scripts
    This is enhancement. With this change converting from and to HTTPS
    will be possible.

---
+++ bash_it_custom/plugins/my_git.plugins.bash
@@ -0,0 +1,88 @@
+cite about-plugin
+about-plugin 'git helper functions'
+
+
+function git_convert_https_clone_to_ssh() {
+# Convert HTTPS github clones to use SSH
+# https://gist.github.com/m14t/3056747
+
+  #-- Script to automate https://help.github.com/articles/why-is-git-always-asking-for-my-password
+
+  REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+  if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using SSH instead of HTTPS."
+    exit
+  fi
+
+  USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
+  if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    exit
+  fi
+
+  REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
+  if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    exit
+  fi
+
+  NEW_URL="git@github.com:$USER/$REPO.git"
+  echo "Changing repo url from "
+  echo "  '$REPO_URL'"
+  echo "      to "
+  echo "  '$NEW_URL'"
+  echo ""
+
+  CHANGE_CMD="git remote set-url origin $NEW_URL"
+  `$CHANGE_CMD`
+
+  echo "Success"
+}
+
+
+
+
+
+function git_convert_ssh_clone_to_https() {
+  #-- Script to automate https://help.github.com/articles/why-is-git-always-asking-for-my-password
+  # Forked from the original to do the opposite:  Switch ssh repo urls to https
+  # Original here: https://gist.github.com/m14t/3056747
+  # Thanks to @m14t
+
+  #origin or upstream
+  REMOTE=${1-origin}
+
+  REPO_URL=`git remote -v | grep -m1 "^$REMOTE" | sed -Ene's#.*(git@github.com:[^[:space:]]*).*#\1#p'`
+  if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using HTTPS instead of SSH."
+    exit
+  fi
+
+  USER=`echo $REPO_URL | sed -Ene's#git@github.com:([^/]*)/(.*).git#\1#p'`
+  if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    exit
+  fi
+
+  REPO=`echo $REPO_URL | sed -Ene's#git@github.com:([^/]*)/(.*).git#\2#p'`
+  if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    exit
+  fi
+
+  #NEW_URL="git@github.com:$USER/$REPO.git"
+  NEW_URL="https://github.com/$USER/$REPO.git"
+  echo "Changing repo url from "
+  echo "  '$REPO_URL'"
+  echo "      to "
+  echo "  '$NEW_URL'"
+  echo ""
+
+  CHANGE_CMD="git remote set-url $REMOTE $NEW_URL"
+  echo "$CHANGE_CMD"
+  `$CHANGE_CMD`
+
+  echo "Success"
+}
