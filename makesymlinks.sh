d00d02df69486b7930ce9c00e1b81e0bda0dff03
Boyko Boykov <boyko@receipt-bank.com>
2017-12-28 15:17:36 +0200

    Refactor makesymlinks.sh
    Making the list more easy to use and uppercasing the global vars

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,36 +1,40 @@
-#!/bin/bash
-############################
+#!/usr/bin/env bash
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/dotfiles                        # dotfiles directory
-olddir=~/dotfiles_old                 # old dotfiles backup directory
-# list of files/folders to symlink in homedir
-files="bash_profile bashrc tmux.conf tmux-osx.conf tmux-linux.conf bash_it_custom inputrc gitconfig gitmessage.txt gitignore_global"
-
-
-##########
+# dotfiles directory
+DOTFILES_DIR=~/dotfiles
+# old dotfiles backup directory
+OLD_DOTFILES_BKP_DIR=~/.old_dotfiles_$(date +%Y%m%d_%H%M%S)
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+echo -n "Creating $OLD_DOTFILES_BKP_DIR for backup of any pre-existing dotfiles ... "
+mkdir -p $OLD_DOTFILES_BKP_DIR
 echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
+echo -n "Changing to the $DOTFILES_DIR directory ..."
+cd $DOTFILES_DIR
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory,
+# move any pre-existing dotfiles in homedir to dotfiles_old directory,
 # then create symlinks from the homedir to any files in the ~/dotfiles
 # directory specified in $files
-for file in $files; do
+while read file
+do
     if [ ! -h ~/.$file ]; then # Move only if file is not already a symlink
-      echo "Moving any existing dotfiles from ~ to $olddir"
-      mv ~/.$file ~/dotfiles_old/
+      echo "Moving any pre-existing dotfiles from ~ to $OLD_DOTFILES_BKP_DIR"
+      mv ~/.$file $OLD_DOTFILES_BKP_DIR
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $DOTFILES_DIR/$file ~/.$file
     fi
-done
+# list of files/folders to symlink in homedir in the below
+done << EOL
+bash_profile
+bashrc
+tmux.conf
+inputrc
+gitconfig
+gitmessage.txt
+gitignore_global
+bash_it_custom
+EOL
