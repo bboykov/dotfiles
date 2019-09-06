47ae924887584ed48bdde33363c3c093f6735568
Boyko Boykov <bboykov@users.noreply.github.com>
2019-09-06 21:02:28 +0300

    Add cnote.sh

--- bin/cnote.sh
+++ bin/cnote.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
+# Create a note file with timestamp and top heading
 
 filename="$1-$(date +%Y-%m-%d).md"
 
 if [[ ! -f $filename ]]; then
 
-touch "$filename"
 cat << EOF > "$filename"
 # $filename
-
 EOF
 
 vim "$filename"
 
 else
-  echo "File exists"
+  echo "Note exists."
   exit 0
 fi
 
