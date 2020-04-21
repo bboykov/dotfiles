414cc1ae8b14ce3b72571b28c3296ab85a8db96d
Boyko Boykov <boyko.boykov@gmail.com>
2020-04-21 09:51:40 +0300

    Improve cnote.sh part 2

--- bin/cnote.sh
+++ bin/cnote.sh
@@ -4,8 +4,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-FILEROOT="$1"
-FILENAME="${FILEROOT}-$(date +%Y-%m-%d).md"
+_JOURNAL=0
 
 case "$OSTYPE" in
   linux*)
@@ -16,10 +15,53 @@ case "$OSTYPE" in
     ;;
 esac
 
+POSITIONAL=()
+while [[ ${#} -gt 0 ]]
+do
+  __option="${1:-}"
+  case "${__option}" in
+    -j|--journal)
+      _JOURNAL=1
+      ;;
+    -*)
+      printf "Unexpected option: %s\\n" "${__option}"
+      exit 1
+      ;;
+    *)
+      POSITIONAL+=("${__option}")
+      ;;
+  esac
+  shift
+done
+
+set -- "${POSITIONAL[@]}"
+if [ $# -gt 1 ]; then
+  printf "Too many positional parameters"
+  exit 1
+fi
+
+FILEROOT="${1-note}"
+
+if [[ _JOURNAL -eq 1 ]]; then
+  FILENAME="$(date +%d-%m-%Y).md"
+
+  read -r -d '' FILE_BODY << EOM || true
+# $(date +%a) ${FILENAME%.*} ${FILEROOT}
+
+EOM
+else
+  FILENAME="${FILEROOT}-$(date +%Y-%m-%d).md"
+
+  read -r -d '' FILE_BODY << EOM || true
+# ${FILEROOT}
+
+EOM
+fi
+
 if [[ ! -f ${FILENAME} ]]; then
 
   cat <<EOF >"${FILENAME}"
-# ${FILEROOT}
+${FILE_BODY}
 EOF
 
   echo "${FILENAME}" | ${COPY_TO_CLIPBOARD}
