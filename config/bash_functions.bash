349e6c3b8f9274b8db06b279417d164ed185ed33
Boyko Boykov <boyko.boykov@gmail.com>
2021-05-13 16:24:22 +0300

    Add k3d and update kind

--- config/bash_functions.bash
+++ config/bash_functions.bash
@@ -18,3 +18,48 @@ prompt_k8s_off() {
 reload() {
   source "${HOME}/.bash_profile"
 }
+
+kind_create_local_kind_cluster() {
+  kind create cluster \
+    --kubeconfig ~/.kube/config \
+    --config $HOME/dotfiles/config/k8s/local-kind-cluster.yaml \
+    --wait 5m \
+    --name local-kind-cluster
+}
+
+kind_delete_local_kind_cluster() {
+  kind delete cluster --name local-kind-cluster
+}
+
+kind_create_local_multi_node_kind_cluster() {
+  kind create cluster \
+    --kubeconfig ~/.kube/config \
+    --config $HOME/dotfiles/config/k8s/local-multi-node-kind-cluster.yaml \
+    --wait 5m \
+    --name local-multi-node-kind-cluster
+}
+
+kind_delete_local_multi_node_kind_cluster() {
+  kind delete cluster --name local-multi-node-kind-cluster
+}
+
+k3d_create_local_k3d_cluster() {
+  export KUBECONFIG="$HOME/.kube/config"
+  k3d cluster create --config "${HOME}/dotfiles/config/k8s/local-k3d-cluster.yaml"
+}
+
+k3d_delete_local_k3d_cluster() {
+  export KUBECONFIG="$HOME/.kube/config"
+  k3d cluster delete local-k3d-cluster
+}
+
+k3d_create_local_multi_node_k3d_cluster() {
+  export KUBECONFIG="$HOME/.kube/config"
+  k3d cluster create --config "${HOME}/dotfiles/config/k8s/local-multi-node-k3d-cluster.yaml"
+}
+
+k3d_delete_local_multi_node_k3d_cluster() {
+  export KUBECONFIG="$HOME/.kube/config"
+  k3d cluster delete local-multi-node-k3d-cluster
+}
+
